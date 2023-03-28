bd817ec1858076fc0335dcbc879a1e1b4949b462
FaresFeso <87316935+FaresFeso@users.noreply.github.com>
2023-03-28 03:57:42 +0200

    Update PlayersScript.lua

--- PlayersScript.lua
+++ PlayersScript.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Fares.0.1", "BloodTheme")
+local Window = Library.CreateLib("SoloX.0.3", "BloodTheme")
 
 -- Players
 local Players = Window:NewTab("Players")
